f7001bdae02ef5f9ec61bc373130ce741c1c3c78
objectiveAC <choiaaron2008@gmail.com>
2025-03-25 21:26:56 +0900

    modified:   app/(tabs)/profile.tsx 	modified:   ios/BowlBuddy.xcodeproj/project.pbxproj 	modified:   ios/Podfile.lock

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -30,11 +30,19 @@ const Profile = () => {
 
       {/* Border */}
       <View className="h-[1px] bg-tertiary mt-[8px]" />
-      <Text className='text-tertiary text-2xl font-gBold'>{user?.displayName}</Text>
-      <TouchableOpacity onPress={SignOut}>
-        <Image source={icons.logout} className='w-14 h-14 p-2' style={{tintColor: '#cccfff'}} resizeMode='contain' />
-      </TouchableOpacity>
+      <View className='flex-1 p-4 mt-2'>
+        <Text className='text-tertiary text-3xl font-gBook pb-5'>{user?.displayName}</Text>
+        <View className='flex-row bg-primary h-96 p-2 rounded-lg border-secondary border-2 shadow-lg' >
 
+        </View>
+        <TouchableOpacity onPress={SignOut}>
+          <View className='flex-row items-center shadow-lg shadow-red-700'>
+            <Image source={icons.logout} className='w-14 h-14 p-3 mt-2 shadow-lg shadow-[#cd1c18]' style={{tintColor: '#cd1c18'}} resizeMode='contain' />
+            <Text className='flex-1 pt-2 text-xl text-[#cd1c18] font-gBold'>Logout</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      
     
     </SafeAreaView>
   )
